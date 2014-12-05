220b27cbe53ba3a48f771312c91b5e83b999474c
Lee suhyun <suhyun@Lees-MacBook-Pro.local>
2014-12-05 11:22:22 +0900

    modify watch event handler modify select option text to title case

--- public/js/app.js
+++ public/js/app.js
@@ -36,21 +36,38 @@ mainApp.directive('timeperiodAddRow', function() {
 			scope.bar4 = stigma2.getConfiguration().timeperiod.minite;
 
 			scope.timeperiodData.foo1 = scope.foo1[0];
-			scope.timeperiodData.foo2 = scope.foo2[5];
+			scope.timeperiodData.foo2 = scope.foo2[0];
+			scope.timeperiodData.foo3 = scope.foo3[0];
 			scope.timeperiodData.bar1 = scope.bar1[0];
 			scope.timeperiodData.bar2 = scope.bar2[0];
 			scope.timeperiodData.bar3 = scope.bar3[0];
 			scope.timeperiodData.bar4 = scope.bar4[0];
 
+			var monthObj = new Object();
+			var weekObj = new Object();
+
+			for (var i in stigma2.getConfiguration().timeperiod.month) {
+				var month = stigma2.getConfiguration().timeperiod.month[i];
+				monthObj[month.text] = month.value;
+			}
+			for (var i in stigma2.getConfiguration().timeperiod.week) {
+				var week = stigma2.getConfiguration().timeperiod.week[i];
+				weekObj[week.text] = week.value;
+			}
+
 			scope.$watch("timeperiodData.foo1", function(newValue, oldValue) {
 				if (newValue === oldValue) return;
-				scope.isWeek = false;
-				for (var i in stigma2.getConfiguration().timeperiod.week) {
-					var week = stigma2.getConfiguration().timeperiod.week[i].value;
-					if (newValue.value === week) {
-						scope.isWeek = true;
-						break;
-					}
+				
+				if (monthObj.hasOwnProperty(oldValue.text) && weekObj.hasOwnProperty(newValue.text)) {
+					scope.isWeek = true;
+					scope.foo2 = stigma2.getConfiguration().timeperiod.weekday;
+					scope.timeperiodData.foo2 = scope.foo2[1];
+					scope.timeperiodData.foo3 = scope.foo3[0];
+				} else if (weekObj.hasOwnProperty(oldValue.text) && monthObj.hasOwnProperty(newValue.text)) {
+					scope.isWeek = false;
+					scope.foo2 = stigma2.getConfiguration().timeperiod.day;
+					scope.timeperiodData.foo2 = scope.foo2[0];
+					scope.timeperiodData.foo3 = "";
 				}
 			});
 		}
